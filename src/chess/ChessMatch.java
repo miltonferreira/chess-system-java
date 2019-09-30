a73402f878a1a8025ab3e9b10715c02dab23fc82
miltonferreira <jotonferreira@gmail.com>
2019-09-30 08:02:20 -0300

    movimento En Passant do peao

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -23,6 +23,7 @@ public class ChessMatch {
 	private Board board;
 	private boolean check; // indica se o rei está em check
 	private boolean checkMate; // indica se rei tomou check mate
+	private ChessPiece enPassantVulnerable; // movimento especial do peao
 
 	private List<Piece> piecesOnTheBoard = new ArrayList<>(); // peças no tabuleiro
 	private List<Piece> capturedPieces = new ArrayList<>(); // peças capturadas
@@ -50,6 +51,10 @@ public class ChessMatch {
 
 	public boolean getCheckMate() {
 		return checkMate;
+	}
+
+	public ChessPiece getEnPassantVulnerable() {
+		return enPassantVulnerable;
 	}
 
 	// retorna uma matriz de peças de xadrez da partida
@@ -95,6 +100,8 @@ public class ChessMatch {
 			throw new ChessException("You can't put yourself in check");
 		}
 
+		ChessPiece movedPiece = (ChessPiece) board.piece(target); // pega uma referencia do destino da peça
+
 		check = (testCheck(opponent(currentPlayer))) ? true : false; // check se o oponente ficou em cheque
 
 		// checa se o oponente ficou em check mate
@@ -102,6 +109,14 @@ public class ChessMatch {
 			checkMate = true;
 		} else {
 			nextTurn(); // chama proximo turno se nao houve check mate
+		}
+
+		// se foi um peao que movimento 2 casas, ele pode tomar en passant
+		if (movedPiece instanceof Pawn
+				&& (target.getRow() == source.getRow() - 2 || target.getRow() == source.getRow() + 2)) {
+			enPassantVulnerable = movedPiece;
+		} else {
+			enPassantVulnerable = null;
 		}
 
 		return (ChessPiece) capturedPiece; // faz um downcast pois é tipo Piece, e nao uma peça do xadrez
@@ -147,6 +162,26 @@ public class ChessMatch {
 
 		}
 
+		// movimento especial en passant do peao
+		if (p instanceof Pawn) {
+			if (source.getColumn() != target.getColumn() && capturedPiece == null) {
+
+				Position pawnPosition;
+
+				if (p.getColor() == Color.WHITE) {
+					pawnPosition = new Position(target.getRow() + 1, target.getColumn()); // posicao da peça que vai ser
+																							// capturada
+				} else {
+					pawnPosition = new Position(target.getRow() - 1, target.getColumn()); // posicao da peça que vai ser
+																							// capturada
+				}
+
+				capturedPiece = board.removePiece(pawnPosition); // remove a peça capturada do tabuleiro
+				capturedPieces.add(capturedPiece); // adiciona a peça capturada a lista
+				piecesOnTheBoard.remove(capturedPiece); // remove a peça da lista de peças do tabuleiro
+			}
+		}
+
 		return capturedPiece; // retorna a peça capturada
 	}
 
@@ -186,6 +221,24 @@ public class ChessMatch {
 			board.placePiece(rook, sourceT); // coloca a torre na nova posicao
 			rook.decreaseMoveCount(); // decrementa -1 movimento
 
+		}
+
+		// movimento especial en passant do peao
+		if (p instanceof Pawn) {
+			if (source.getColumn() != target.getColumn() && capturedPiece == enPassantVulnerable) {
+				
+				ChessPiece pawn = (ChessPiece)board.removePiece(target);	//pega peao que está na posicao errada, uma casa atrás
+				Position pawnPosition;
+
+				if (p.getColor() == Color.WHITE) {
+					pawnPosition = new Position(3, target.getColumn()); // posicao da peça anterior a jogada
+				} else {
+					pawnPosition = new Position(4, target.getColumn()); // posicao da peça anterior a jogada
+				}
+				
+				board.placePiece(pawn, pawnPosition); // posiciona a peça na casa anterior a jogada
+				
+			}
 		}
 
 	}
@@ -319,14 +372,14 @@ public class ChessMatch {
 		placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
 		placeNewPiece('g', 1, new Knight(board, Color.WHITE));
 		placeNewPiece('h', 1, new Rook(board, Color.WHITE));
-		placeNewPiece('a', 2, new Pawn(board, Color.WHITE));
-		placeNewPiece('b', 2, new Pawn(board, Color.WHITE));
-		placeNewPiece('c', 2, new Pawn(board, Color.WHITE));
-		placeNewPiece('d', 2, new Pawn(board, Color.WHITE));
-		placeNewPiece('e', 2, new Pawn(board, Color.WHITE));
-		placeNewPiece('f', 2, new Pawn(board, Color.WHITE));
-		placeNewPiece('g', 2, new Pawn(board, Color.WHITE));
-		placeNewPiece('h', 2, new Pawn(board, Color.WHITE));
+		placeNewPiece('a', 2, new Pawn(board, Color.WHITE, this));
+		placeNewPiece('b', 2, new Pawn(board, Color.WHITE, this));
+		placeNewPiece('c', 2, new Pawn(board, Color.WHITE, this));
+		placeNewPiece('d', 2, new Pawn(board, Color.WHITE, this));
+		placeNewPiece('e', 2, new Pawn(board, Color.WHITE, this));
+		placeNewPiece('f', 2, new Pawn(board, Color.WHITE, this));
+		placeNewPiece('g', 2, new Pawn(board, Color.WHITE, this));
+		placeNewPiece('h', 2, new Pawn(board, Color.WHITE, this));
 
 		placeNewPiece('a', 8, new Rook(board, Color.BLACK));
 		placeNewPiece('b', 8, new Knight(board, Color.BLACK));
@@ -336,14 +389,14 @@ public class ChessMatch {
 		placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
 		placeNewPiece('g', 8, new Knight(board, Color.BLACK));
 		placeNewPiece('h', 8, new Rook(board, Color.BLACK));
-		placeNewPiece('a', 7, new Pawn(board, Color.BLACK));
-		placeNewPiece('b', 7, new Pawn(board, Color.BLACK));
-		placeNewPiece('c', 7, new Pawn(board, Color.BLACK));
-		placeNewPiece('d', 7, new Pawn(board, Color.BLACK));
-		placeNewPiece('e', 7, new Pawn(board, Color.BLACK));
-		placeNewPiece('f', 7, new Pawn(board, Color.BLACK));
-		placeNewPiece('g', 7, new Pawn(board, Color.BLACK));
-		placeNewPiece('h', 7, new Pawn(board, Color.BLACK));
+		placeNewPiece('a', 7, new Pawn(board, Color.BLACK, this));
+		placeNewPiece('b', 7, new Pawn(board, Color.BLACK, this));
+		placeNewPiece('c', 7, new Pawn(board, Color.BLACK, this));
+		placeNewPiece('d', 7, new Pawn(board, Color.BLACK, this));
+		placeNewPiece('e', 7, new Pawn(board, Color.BLACK, this));
+		placeNewPiece('f', 7, new Pawn(board, Color.BLACK, this));
+		placeNewPiece('g', 7, new Pawn(board, Color.BLACK, this));
+		placeNewPiece('h', 7, new Pawn(board, Color.BLACK, this));
 
 	}
 
--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -2,14 +2,17 @@ package chess.pieces;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.Color;
 
 public class Pawn extends ChessPiece {
 
-	public Pawn(Board board, Color color) {
-		super(board, color);
+	private ChessMatch chessMatch;
 
+	public Pawn(Board board, Color color, ChessMatch chessMatch) {
+		super(board, color);
+		this.chessMatch = chessMatch;
 	}
 
 	@Override
@@ -22,70 +25,113 @@ public class Pawn extends ChessPiece {
 
 		// anda para cima, linhas a menos por ser branca
 		if (getColor() == Color.WHITE) {
-			
-			p.setValue(position.getRow() - 1, position.getColumn());					// recebe nova linha +1/coluna
+
+			p.setValue(position.getRow() - 1, position.getColumn()); // recebe nova linha +1/coluna
 
 			// verifica se a posicao existe e nao tem peça na posiçao
 			if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
-				mat[p.getRow()][p.getColumn()] = true; 									// a peça pode mover para a posicao
-			}
-
-			p.setValue(position.getRow() - 2, position.getColumn());					// recebe nova linha +2/coluna
-			Position p2 = new Position(position.getRow() - 1, position.getColumn());	// pega posicao a frente do peao
-
-			// verifica se a posicao existe e nao tem peça na posiçao e o contador igual 0
-			if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p) && getMoveCount() == 0 && getBoard().positionExists(p2) && !getBoard().thereIsAPiece(p2)) {
 				mat[p.getRow()][p.getColumn()] = true; // a peça pode mover para a posicao
 			}
-			
-			// movimento de capturar peça do oponente ------------------------------------------------------
+
+			p.setValue(position.getRow() - 2, position.getColumn()); // recebe nova linha +2/coluna
+			Position p2 = new Position(position.getRow() - 1, position.getColumn()); // pega posicao a frente do peao
+
+			// verifica se a posicao existe e nao tem peça na posiçao e o contador igual 0
+			if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p) && getMoveCount() == 0
+					&& getBoard().positionExists(p2) && !getBoard().thereIsAPiece(p2)) {
+				mat[p.getRow()][p.getColumn()] = true; // a peça pode mover para a posicao
+			}
+
+			// movimento de capturar peça do oponente
+			// ------------------------------------------------------
 			// esquerda
-			p.setValue(position.getRow() - 1, position.getColumn() - 1);				// recebe nova linha +1/coluna
+			p.setValue(position.getRow() - 1, position.getColumn() - 1); // recebe nova linha +1/coluna
 
 			// verifica se a posicao existe e tem oponente
 			if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
-				mat[p.getRow()][p.getColumn()] = true; 									// a peça pode mover para a posicao
+				mat[p.getRow()][p.getColumn()] = true; // a peça pode mover para a posicao
 			}
 			// direita
-			p.setValue(position.getRow() - 1, position.getColumn() + 1);				// recebe nova linha +1/coluna
+			p.setValue(position.getRow() - 1, position.getColumn() + 1); // recebe nova linha +1/coluna
 
 			// verifica se a posicao existe e tem oponente
 			if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
-				mat[p.getRow()][p.getColumn()] = true; 									// a peça pode mover para a posicao
+				mat[p.getRow()][p.getColumn()] = true; // a peça pode mover para a posicao
 			}
-			
-		}else{
+
+			// movimento especial en passant para peça branca
+			if (position.getRow() == 3) {
+
+				Position left = new Position(position.getRow(), position.getColumn() - 1); // pega posicao da casa a
+																							// esquerda
+				if (getBoard().positionExists(left) && isThereOpponentPiece(left)
+						&& getBoard().piece(left) == chessMatch.getEnPassantVulnerable()) {
+					mat[left.getRow() - 1][left.getColumn()] = true; // inclue movimento possivel ao peao a esquerda
+				}
+
+				Position right = new Position(position.getRow(), position.getColumn() + 1); // pega posicao da casa a
+																							// esquerda
+				if (getBoard().positionExists(right) && isThereOpponentPiece(right)
+						&& getBoard().piece(right) == chessMatch.getEnPassantVulnerable()) {
+					mat[right.getRow() - 1][right.getColumn()] = true; // inclue movimento possivel ao peao a esquerda
+				}
+
+			}
+
+		} else {
 			// peça preta ------------------------------------------------
-			p.setValue(position.getRow() + 1, position.getColumn());					// recebe nova linha +1/coluna
+			p.setValue(position.getRow() + 1, position.getColumn()); // recebe nova linha +1/coluna
 
 			// verifica se a posicao existe e nao tem peça na posiçao
 			if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
-				mat[p.getRow()][p.getColumn()] = true; 									// a peça pode mover para a posicao
-			}
-
-			p.setValue(position.getRow() + 2, position.getColumn());					// recebe nova linha +2/coluna
-			Position p2 = new Position(position.getRow() + 1, position.getColumn());	// pega posicao a frente do peao
-
-			// verifica se a posicao existe e nao tem peça na posiçao e o contador igual 0
-			if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p) && getMoveCount() == 0 && getBoard().positionExists(p2) && !getBoard().thereIsAPiece(p2)) {
 				mat[p.getRow()][p.getColumn()] = true; // a peça pode mover para a posicao
 			}
-			
-			// movimento de capturar peça do oponente ------------------------------------------------------
+
+			p.setValue(position.getRow() + 2, position.getColumn()); // recebe nova linha +2/coluna
+			Position p2 = new Position(position.getRow() + 1, position.getColumn()); // pega posicao a frente do peao
+
+			// verifica se a posicao existe e nao tem peça na posiçao e o contador igual 0
+			if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p) && getMoveCount() == 0
+					&& getBoard().positionExists(p2) && !getBoard().thereIsAPiece(p2)) {
+				mat[p.getRow()][p.getColumn()] = true; // a peça pode mover para a posicao
+			}
+
+			// movimento de capturar peça do oponente
+			// ------------------------------------------------------
 			// esquerda
-			p.setValue(position.getRow() + 1, position.getColumn() - 1);				// recebe nova linha +1/coluna
+			p.setValue(position.getRow() + 1, position.getColumn() - 1); // recebe nova linha +1/coluna
 
 			// verifica se a posicao existe e tem oponente
 			if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
-				mat[p.getRow()][p.getColumn()] = true; 									// a peça pode mover para a posicao
+				mat[p.getRow()][p.getColumn()] = true; // a peça pode mover para a posicao
 			}
 			// direita
-			p.setValue(position.getRow() + 1, position.getColumn() + 1);				// recebe nova linha +1/coluna
+			p.setValue(position.getRow() + 1, position.getColumn() + 1); // recebe nova linha +1/coluna
 
 			// verifica se a posicao existe e tem oponente
 			if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
-				mat[p.getRow()][p.getColumn()] = true; 									// a peça pode mover para a posicao
+				mat[p.getRow()][p.getColumn()] = true; // a peça pode mover para a posicao
 			}
+
+			// movimento especial en passant para peça preta
+			if (position.getRow() == 4) {
+
+				Position left = new Position(position.getRow(), position.getColumn() - 1); // pega posicao da casa a esquerda
+																							
+				if (getBoard().positionExists(left) && isThereOpponentPiece(left)
+						&& getBoard().piece(left) == chessMatch.getEnPassantVulnerable()) {
+					mat[left.getRow() + 1][left.getColumn()] = true; // inclue movimento possivel ao peao a esquerda
+				}
+
+				Position right = new Position(position.getRow(), position.getColumn() + 1); // pega posicao da casa a esquerda
+																							
+				if (getBoard().positionExists(right) && isThereOpponentPiece(right)
+						&& getBoard().piece(right) == chessMatch.getEnPassantVulnerable()) {
+					mat[right.getRow() + 1][right.getColumn()] = true; // inclue movimento possivel ao peao a esquerda
+				}
+
+			}
+
 		}
 
 		return mat;
@@ -95,7 +141,5 @@ public class Pawn extends ChessPiece {
 	public String toString() {
 		return "P";
 	}
-	
-	
 
 }
